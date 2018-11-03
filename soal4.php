6e43d8d26a557f0392d6e8421ce480a17fe478bb
xmazel <30872605+xmazel@users.noreply.github.com>
2018-11-03 19:01:01 +0700

    Update soal4.php

--- soal4.php
+++ soal4.php
@@ -7,7 +7,27 @@ function isBentrok($a, $b){
   		return ( $a / fpb($a,$b) ) * $b; 
 	}
 
+?>
 
-echo "Jadwal Pak Hasan akan bentrok setiap <b>" .isBentrok(4,5). "</b> hari sekali";
+
+<form action="" method="POST">
+
+	Masukkan jadwal di Amarta<input type="text" name="amarta"><br>
+	Masukkan jadwal di Gor<input type="text" name="gor"><br>
+	<input type="submit" name="submit">
+</form>
+
+
+<?php
+if (isset($_POST['submit'])) {
+	$amarta = $_POST['amarta'];
+	$gor = $_POST['gor'];
+
+	echo "Jadwal Pak Hasan akan bentrok setiap <b>" .isBentrok($amarta,$gor). "</b> hari sekali";
+
+}
+
+
+
 
  ?>
