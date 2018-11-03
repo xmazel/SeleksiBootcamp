e80439dcc983e5f621f9692810e9b7d051213eee
xmazel <30872605+xmazel@users.noreply.github.com>
2018-11-03 18:53:54 +0700

    Update soal5.php

--- soal5.php
+++ soal5.php
@@ -1,12 +1,25 @@
 <?php
-
-
-
 function countChar($string, $char){
 	$result = substr_count($string, $char);
 
 	return $result;
 }
-$res = countChar("Bootcamp","o") ;
-echo $string."Character " .$char. " found, used <b><i>" .$res. "</i></b> times";
+
 ?>
+<form action="" method="POST">
+	<br>
+<label>Masukkan String</label><input type="text" name="inputString"><br>
+<label>Masukkan Char</label><input type="text" name="inputChar"><br>
+<input type="submit" name="submit">
+</form>
+
+<?php
+if (isset($_POST['submit'])) {
+$str = $_POST['inputString'];
+$chr = $_POST['inputChar'];
+$res = countChar($str,$chr) ;
+
+echo $str." Character '<b>" .$chr. "</b>' found, used <b><i>" .$res. "</i></b> times";
+}
+?>
+
