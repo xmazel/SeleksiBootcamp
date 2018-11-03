3a40d19a00429201f196e1a6558c65606a46ef18
xmazel <30872605+xmazel@users.noreply.github.com>
2018-11-03 19:18:54 +0700

    Update soal2.php

--- soal2.php
+++ soal2.php
@@ -5,14 +5,16 @@
 </head>
 <body>
 	<form action="" method="POST">
-		<br>
+	<br>
 	Masukkan username :
 	<input type="text" name="try" size="10" pattern="[a-zA-Z]{5}_[0-9]{2}" title="5 huruf kecil/besar dilanjutkan tanda '_' atau underscore lanjut dua nomor">
 	<input class="btn btn-primary" type="submit" name="submit">
 	</form>
 <?php
-if (isset($_POST["check"])) {
-	echo "Submit OK";
+
+if (isset($_POST["submit"])) {
+	echo $_POST['try'];
+	echo "<script>alert('OK')</script>";
 }
 
 ?>
