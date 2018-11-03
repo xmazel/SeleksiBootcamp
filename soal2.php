17881af7ba1b9deaaf122f35066f3dbbc0c4a9df
xmazel <30872605+xmazel@users.noreply.github.com>
2018-11-03 18:22:43 +0700

    Update soal2.php

--- soal2.php
+++ soal2.php
@@ -5,6 +5,8 @@
 </head>
 <body>
 	<form action="" method="POST">
+		<br>
+	Masukkan username :
 	<input type="text" name="try" size="10" pattern="[a-zA-Z]{5}_[0-9]{2}" title="5 huruf kecil/besar dilanjutkan tanda '_' atau underscore lanjut dua nomor">
 	<input type="submit" name="check">
 	</form>
